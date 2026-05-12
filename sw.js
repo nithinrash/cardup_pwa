d509316a9406b3b68f3089608a08ad991a6c49b9
nithinrash <nkrash91@gmail.com>
2026-05-12 15:37:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
-const CACHE_NAME = 'cardup-v1';
+const CACHE_NAME = 'cardup-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  // Force this new version to activate immediately
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+  // Delete old caches (v1, v2)
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
